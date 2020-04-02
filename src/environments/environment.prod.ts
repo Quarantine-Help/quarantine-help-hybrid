c60875785b5483cfbd19c6412a67097e79b2e10c
Kuber <kuber@4demkuber1.ad.here.com>
2020-04-02 10:35:27 +0200

    changed apikey

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   JS_KEY : "iY3ZFYqYj9Di2O4fuht-ilB7gONXpsYceJIsDgDbr94",
-  REST_KEY : "OcuKqEid5uu0nrC8zIAgEYx_VbsAvl9roMp1ihJRJl_aykX8D3CeGnJ3fXcvitabqLpRRvnn9iO81lmAV5HoJg"
+  REST_KEY : "INxGhspY9TqShx3heSZSBmobOsutPeE9eJaTxfHiiQQ"
 };
