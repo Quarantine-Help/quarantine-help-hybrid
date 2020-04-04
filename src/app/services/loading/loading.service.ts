843f61442db3eacdc0011988b712ec35fe379a3c
Sebin  Benjamin <sebinbenjamin@gmail.com>
2020-04-05 01:56:55 +1300

    feat: add services for geolocation, loading animation and toast

--- src/app/services/loading/loading.service.ts
+++ src/app/services/loading/loading.service.ts
@@ -5,28 +5,19 @@ import { LoadingController } from '@ionic/angular';
   providedIn: 'root',
 })
 export class LoadingService {
-  constructor(public loadingController: LoadingController) {}
+  constructor(private loadingController: LoadingController) {}
 
-  async presentLoadingWithOptions(options?) {
+  presentLoadingWithOptions(options?): Promise<HTMLIonLoadingElement> {
     // Spinner types include : "bubbles" | "circles" | "circular" | "crescent" | "dots" | "lines" | "lines-small"
-    // Refer https://ionicframework.com/docs/api/loading to find out
-    // the default values and only add the diff. Also the see ...(rest
+    // Refer https://ionicframework.com/docs/api/loading to find out the default values and only add the diff. Also the see ...(rest
     // operator) is used here and will replace the existing value.
-    let loadingElement: HTMLIonLoadingElement;
-    try {
-      loadingElement = await this.loadingController.create({
-        spinner: 'bubbles',
-        duration: 5000,
-        message: 'Loader will dismiss in 5 seconds...',
-        translucent: true,
-        cssClass: 'custom-loading',
-        ...options,
-      });
-      await loadingElement.present();
-    } catch (error) {
-      console.error('Error loading loader :D', error);
-      loadingElement = undefined;
-    }
-    return loadingElement;
+    return this.loadingController.create({
+      spinner: 'bubbles',
+      duration: 5000,
+      message: 'Loader will dismiss in 5 seconds...',
+      translucent: true,
+      cssClass: 'custom-loading',
+      ...options,
+    });
   }
 }
