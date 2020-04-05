38a9aeafceed7dea307c6bcc8cc41c2028e2c91e
Sebin  Benjamin <sebinbenjamin@gmail.com>
2020-04-05 23:26:36 +1200

    feat: change titlebar color and splashscreen duration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,28 @@
 import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Plugins, StatusBarStyle } from '@capacitor/core';
+
+const { StatusBar, SplashScreen } = Plugins;
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
+  constructor(private platform: Platform) {
+    SplashScreen.show({
+      autoHide: true,
+    });
     this.initializeApp();
   }
 
   initializeApp() {
-    this.platform.ready().then(readySource => {
-      if (readySource !== 'dom') {
-        this.statusBar.styleDefault();
-        this.splashScreen.hide();
+    this.platform.ready().then(() => {
+      if (this.platform.is('hybrid')) {
+        StatusBar.setBackgroundColor({ color: 'white' });
+        StatusBar.setStyle({ style: StatusBarStyle.Light });
+        SplashScreen.hide();
       }
     });
   }
