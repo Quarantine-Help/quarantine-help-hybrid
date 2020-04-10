63b1187f3bf967d471b99460d7b4e8735180faf0
Sebin  Benjamin <sebinbenjamin@gmail.com>
2020-04-10 20:41:34 +1200

    feat: build login page UI components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,20 +2,25 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'quarantine-map', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   {
     path: 'quarantine-map',
     loadChildren: () =>
       import('./pages/quarantine-map/quarantine-map.module').then(
-        m => m.QuarantineMapPageModule
-      )
-  }
+        (m) => m.QuarantineMapPageModule
+      ),
+  },
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./pages/login/login.module').then((m) => m.LoginPageModule),
+  },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
