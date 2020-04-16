1bbc287277594615d009c289693a0d44cbfb9779
Nimisha Wilson <nimishawilson123@gmail.com>
2020-04-16 14:55:31 +0530

    feat: add form validation for user registration

--- src/app/pages/user-registration/user-registration.page.ts
+++ src/app/pages/user-registration/user-registration.page.ts
@@ -1,15 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user-registration',
   templateUrl: './user-registration.page.html',
   styleUrls: ['./user-registration.page.scss'],
 })
-export class UserRegistrationPage implements OnInit {
+export class UserRegistrationPage implements OnInit, OnDestroy {
+  registrationForm: FormGroup;
+  showPasswordText: boolean; // To toggle password visibility
+  passwordIcon: 'eye' | 'eye-off' = 'eye';
+  pageClean: boolean; // Flag to check if no changes were made.
+  regSubs: Subscription;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.pageClean = true;
+    this.showPasswordText = false;
+    this.registrationForm = new FormGroup({
+      firstName: new FormControl('', [
+        Validators.required
+      ]),
+      lastName: new FormControl('', [
+        Validators.required
+      ]),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.email,
+      ]),
+      phoneNumber: new FormControl('', [
+        Validators.minLength(8),
+        Validators.maxLength(16),
+        Validators.required
+      ]),
+      password: new FormControl('', [
+        Validators.minLength(8),
+        Validators.maxLength(30),
+        Validators.required
+      ]),
+    });
   }
 
+  ngOnInit() {
+    this.regSubs = this.registrationForm.valueChanges.subscribe((change) => {
+      this.pageClean = false;
+    });
+  }
+
+  ngOnDestroy() {
+    this.regSubs.unsubscribe();
+  }
+
+  togglePasswordVisibility() {
+    this.passwordIcon = this.passwordIcon === 'eye-off' ? 'eye' : 'eye-off';
+    if (this.passwordIcon === 'eye-off') {
+      setTimeout(() => {
+        this.passwordIcon = 'eye';
+      }, 6000);
+    }
+  }
+
+  registerUser() {
+    console.log(this.registrationForm.value);
+  }
 }
