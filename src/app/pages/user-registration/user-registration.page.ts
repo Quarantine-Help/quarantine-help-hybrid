ac639ba16923270d38efaa142c7c8073d743300d
Nimisha Wilson <nimishawilson123@gmail.com>
2020-04-16 22:26:37 +0530

    feat: add form validation for quarantined user registration

--- src/app/pages/user-registration/user-registration.page.ts
+++ src/app/pages/user-registration/user-registration.page.ts
@@ -9,14 +9,32 @@ import { Subscription } from 'rxjs';
 })
 export class UserRegistrationPage implements OnInit, OnDestroy {
   registrationForm: FormGroup;
+  quarantineRegistration: FormGroup;
   showPasswordText: boolean; // To toggle password visibility
   passwordIcon: 'eye' | 'eye-off' = 'eye';
   pageClean: boolean; // Flag to check if no changes were made.
+  pageCleanQuarantined: boolean;
   regSubs: Subscription;
+  regQuarantineSubs: Subscription;
+  showVolunteer: boolean; // Flag for hiding and showing 2 forms
+  quarantinedOrVolunteer: string;
+  setDefault: string;
+
+  segmentChanged(ev: any) {
+    this.quarantinedOrVolunteer = ev.detail.value;
+    if (this.quarantinedOrVolunteer === 'Quarantined') {
+      this.showVolunteer = false;
+    } else if (this.quarantinedOrVolunteer === 'Volunteer') {
+      this.showVolunteer = true;
+    }
+  }
 
   constructor() {
     this.pageClean = true;
+    this.pageCleanQuarantined = true;
     this.showPasswordText = false;
+    this.showVolunteer = true;
+    this.setDefault = 'Volunteer';
     this.registrationForm = new FormGroup({
       firstName: new FormControl('', [
         Validators.required
@@ -40,16 +58,55 @@ export class UserRegistrationPage implements OnInit, OnDestroy {
         Validators.required
       ]),
     });
+    this.quarantineRegistration = new FormGroup({
+      firstName: new FormControl('', [
+        Validators.required
+      ]),
+      lastName: new FormControl('', [
+        Validators.required
+      ]),
+      address: new FormControl('', [
+        Validators.required,
+        Validators.minLength(2)
+      ]),
+      city: new FormControl('', [
+        Validators.required,
+        Validators.minLength(2)
+      ]),
+      country: new FormControl('', [
+        Validators.required,
+        Validators.minLength(2)
+      ]),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.email,
+      ]),
+      phoneNumber: new FormControl('', [
+        Validators.minLength(8),
+        Validators.maxLength(16),
+        Validators.required
+      ]),
+      password: new FormControl('', [
+        Validators.minLength(8),
+        Validators.maxLength(30),
+        Validators.required
+      ]),
+    });
   }
 
   ngOnInit() {
     this.regSubs = this.registrationForm.valueChanges.subscribe((change) => {
       this.pageClean = false;
     });
+    this.regQuarantineSubs = this.quarantineRegistration.valueChanges.subscribe((change) => {
+      this.pageCleanQuarantined = false;
+    });
   }
 
   ngOnDestroy() {
     this.regSubs.unsubscribe();
+    this.regQuarantineSubs.unsubscribe();
   }
 
   togglePasswordVisibility() {
@@ -63,5 +120,9 @@ export class UserRegistrationPage implements OnInit, OnDestroy {
 
   registerUser() {
     console.log(this.registrationForm.value);
+  }
+
+  registerVolunteer() {
+    console.log(this.quarantineRegistration.value);
   }
 }
