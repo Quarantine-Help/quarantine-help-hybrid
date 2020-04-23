e02eb12df338d4eb584592e0b5214efc3a5091f0
Nimisha Wilson <nimishawilson123@gmail.com>
2020-04-23 21:58:44 +0530

    feat: warn users on cancel without save, when form is dirty

--- src/app/pages/user-profile/user-profile.page.ts
+++ src/app/pages/user-profile/user-profile.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators, FormGroup } from '@angular/forms';
 
+import { MiscService } from 'src/app/services/misc/misc.service';
+
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.page.html',
@@ -9,19 +11,10 @@ import { FormControl, Validators, FormGroup } from '@angular/forms';
 export class UserProfilePage implements OnInit {
   quaRegForm: FormGroup;
   volRegForm: FormGroup;
-  isVolunteer: boolean; // flag for showing different forms
+  isVolunteer: boolean; // flag for current user type
   isEditable: boolean;
-  userDetails: {
-    firstName: string;
-    lastName: string;
-    address: string;
-    city: string;
-    country: string;
-    emailid: string;
-    phoneNumber: string;
-  };
 
-  constructor() {
+  constructor(private miscService: MiscService) {
     this.volRegForm = new FormGroup({
       firstName: new FormControl('', [Validators.required]),
       lastName: new FormControl('', [Validators.required]),
@@ -62,9 +55,19 @@ export class UserProfilePage implements OnInit {
   }
 
   ngOnInit() {
+    // TODO: get the user type from backend
     this.isVolunteer = false;
     this.isEditable = false;
-    this.userDetails = {
+    this.syncProfileDetails();
+  }
+
+  onEdit() {
+    this.isEditable = true;
+  }
+
+  syncProfileDetails() {
+    // TODO: get profile details from API and cache it incase user discards changes
+    const userDetails = {
       firstName: 'johny',
       lastName: 'depp',
       address: 'apartment no, xyz street',
@@ -73,34 +76,61 @@ export class UserProfilePage implements OnInit {
       emailid: 'example@gmail.com',
       phoneNumber: '9846512234',
     };
-    this.quaRegForm.get('firstName').setValue(this.userDetails.firstName);
-    this.quaRegForm.get('lastName').setValue(this.userDetails.lastName);
-    this.quaRegForm.get('address').setValue(this.userDetails.address);
-    this.quaRegForm.get('city').setValue(this.userDetails.city);
-    this.quaRegForm.get('country').setValue(this.userDetails.country);
-    this.quaRegForm.get('email').setValue(this.userDetails.emailid);
-    this.quaRegForm.get('phoneNumber').setValue(this.userDetails.phoneNumber);
-    this.volRegForm.get('firstName').setValue(this.userDetails.firstName);
-    this.volRegForm.get('lastName').setValue(this.userDetails.lastName);
-    this.volRegForm.get('email').setValue(this.userDetails.emailid);
-    this.volRegForm.get('phoneNumber').setValue(this.userDetails.phoneNumber);
-  }
-
-  onEdit() {
-    this.isEditable = true;
+    if (this.isVolunteer) {
+      this.volRegForm.get('firstName').setValue(userDetails.firstName);
+      this.volRegForm.get('lastName').setValue(userDetails.lastName);
+      this.volRegForm.get('email').setValue(userDetails.emailid);
+      this.volRegForm.get('phoneNumber').setValue(userDetails.phoneNumber);
+    } else {
+      this.quaRegForm.get('firstName').setValue(userDetails.firstName);
+      this.quaRegForm.get('lastName').setValue(userDetails.lastName);
+      this.quaRegForm.get('address').setValue(userDetails.address);
+      this.quaRegForm.get('city').setValue(userDetails.city);
+      this.quaRegForm.get('country').setValue(userDetails.country);
+      this.quaRegForm.get('email').setValue(userDetails.emailid);
+      this.quaRegForm.get('phoneNumber').setValue(userDetails.phoneNumber);
+    }
   }
 
   onCancel() {
     this.isEditable = false;
+    if (this.quaRegForm.dirty || this.volRegForm.dirty) {
+      this.miscService.presentAlert({
+        header: 'Warning',
+        subHeader: 'Are you sure? ',
+        buttons: [
+          {
+            text: 'Cancel',
+            role: 'cancel',
+            cssClass: 'secondary',
+            handler: () => {
+              this.isEditable = true;
+            },
+          },
+          {
+            text: 'Yes',
+            handler: () => {
+              this.isEditable = false;
+              this.syncProfileDetails();
+              this.volRegForm.markAsPristine();
+              this.quaRegForm.markAsPristine();
+            },
+          },
+        ],
+        message: `Your changes are not saved. Are you sure you want to discard the changes? `,
+      });
+    }
   }
 
   saveUser() {
     this.isEditable = false;
     console.log(this.quaRegForm.value);
+    this.quaRegForm.markAsPristine(); // for checking status of form in further edit and cancel
   }
 
   saveVolUser() {
     this.isEditable = false;
     console.log(this.volRegForm.value);
+    this.volRegForm.markAsPristine(); // for checking status of form in further edit and cancel
   }
 }
