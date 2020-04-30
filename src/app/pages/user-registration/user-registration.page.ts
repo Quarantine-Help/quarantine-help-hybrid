698393e1bae4694c14019077ab8bccd61a86ee5e
Sebin  Benjamin <sebinbenjamin@gmail.com>
2020-05-01 01:35:33 +1200

    feat: add 2nd line for address, a postcode and correct country code

--- src/app/pages/user-registration/user-registration.page.ts
+++ src/app/pages/user-registration/user-registration.page.ts
@@ -1,19 +1,26 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormControl, Validators, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 
 import { GeoLocationService } from 'src/app/services/geo-location/geo-location.service';
 import { MiscService } from 'src/app/services/misc/misc.service';
 import { HEREMapService } from 'src/app/services/HERE-map/here-map.service';
+import { AuthService } from 'src/app/services/auth/auth.service';
 import { LatLng } from '../../models/geo';
 import { ReverseGeoResult } from 'src/app/models/here-map';
-import { Router } from '@angular/router';
+import { UserRegData, UserRegResponse } from 'src/app/models/auth';
+import { UserType } from 'src/app/models/core-api';
+import { Crisis } from 'src/app/constants/core-api';
 
 type userSegments = 'volunteer' | 'quarantined';
 interface UserAddress {
   address: string;
   city: string;
   country: string;
+  countryCode: string;
+  postCode: string;
+  placeId: string;
 }
 @Component({
   selector: 'app-user-registration',
@@ -35,12 +42,14 @@ export class UserRegistrationPage implements OnInit, OnDestroy {
   loadingAniGetAddr: HTMLIonLoadingElement;
   currentLocation: LatLng = undefined;
   userAddress: UserAddress;
+  userRegAni: HTMLIonLoadingElement;
 
   constructor(
     private geoLocationService: GeoLocationService,
     private miscService: MiscService,
     private hereMapService: HEREMapService,
-    private router: Router
+    private router: Router,
+    private authService: AuthService
   ) {
     this.volRegForm = new FormGroup({
       firstName: new FormControl('', [Validators.required]),
@@ -64,11 +73,16 @@ export class UserRegistrationPage implements OnInit, OnDestroy {
     this.quaRegForm = new FormGroup({
       firstName: new FormControl('', [Validators.required]),
       lastName: new FormControl('', [Validators.required]),
-      address: new FormControl('', [
+      address1: new FormControl('', [
+        Validators.required,
+        Validators.minLength(2),
+      ]),
+      address2: new FormControl('', [
         Validators.required,
         Validators.minLength(2),
       ]),
       city: new FormControl('', [Validators.required, Validators.minLength(2)]),
+      postCode: new FormControl('', [Validators.required]),
       country: new FormControl('', [
         Validators.required,
         Validators.minLength(2),
@@ -195,9 +209,10 @@ export class UserRegistrationPage implements OnInit, OnDestroy {
   getUserAddress() {
     // call reverse-geo code iff the user address does not exist
     if (this.userAddress) {
-      this.quaRegForm.get('address').setValue(this.userAddress.address);
+      this.quaRegForm.get('address1').setValue(this.userAddress.address);
       this.quaRegForm.get('city').setValue(this.userAddress.city);
       this.quaRegForm.get('country').setValue(this.userAddress.country);
+      this.quaRegForm.get('countryCode').setValue(this.userAddress.countryCode);
     } else {
       this.miscService
         .presentLoadingWithOptions({
@@ -211,24 +226,30 @@ export class UserRegistrationPage implements OnInit, OnDestroy {
           this.hereMapService
             .getUserAddress(this.currentLocation)
             .then((data: ReverseGeoResult) => {
-              console.log(data);
               // Destroy loading controller on dismiss
               if (this.loadingAniGetAddr !== undefined) {
                 this.loadingAniGetAddr.dismiss().then(() => {
                   this.loadingAniGetAddr = undefined;
                 });
               }
-              const geoDataObj =
-                data.body.Response.View[0].Result[0].Location.Address;
+              const geoDataObj = data.body.Response.View[0].Result[0].Location;
               this.userAddress = {
-                address: geoDataObj.Label,
-                city: geoDataObj.City,
-                country: geoDataObj.AdditionalData[0].value,
+                address: geoDataObj.Address.Label,
+                city: geoDataObj.Address.City,
+                countryCode: geoDataObj.Address.AdditionalData[0].value,
+                country: geoDataObj.Address.AdditionalData[1].value,
+                postCode: geoDataObj.Address.PostalCode,
+                placeId: geoDataObj.LocationId,
               };
               // set the values to the form
-              this.quaRegForm.get('address').setValue(this.userAddress.address);
+              this.quaRegForm
+                .get('address1')
+                .setValue(this.userAddress.address);
               this.quaRegForm.get('city').setValue(this.userAddress.city);
               this.quaRegForm.get('country').setValue(this.userAddress.country);
+              this.quaRegForm
+                .get('postCode')
+                .setValue(this.userAddress.postCode);
             });
         })
         .catch((error) => {
@@ -243,13 +264,90 @@ export class UserRegistrationPage implements OnInit, OnDestroy {
     }
   }
 
-  registerUser() {
-    console.log(this.volRegForm.value);
-    this.router.navigate(['/login']);
+  registerUser(userType: UserType) {
+    const userData: UserRegData = {
+      user: undefined,
+      type: undefined,
+      phone: undefined,
+      placeId: undefined,
+      postCode: undefined,
+      city: undefined,
+      country: undefined,
+      position: undefined,
+      firstLineOfAddress: undefined,
+      secondLineOfAddress: '',
+      crisis: Crisis.COVID19,
+    };
+
+    if (userType === 'AF') {
+      userData.user = {
+        firstName: this.quaRegForm.get('firstName').value,
+        lastName: this.quaRegForm.get('lastName').value,
+        email: this.quaRegForm.get('email').value,
+        password: this.quaRegForm.get('password').value,
+      };
+      userData.city = this.quaRegForm.get('city').value;
+      userData.postCode = this.quaRegForm.get('postCode').value;
+      userData.firstLineOfAddress = this.quaRegForm.get('address1').value;
+      userData.secondLineOfAddress = this.quaRegForm.get('address2').value;
+      userData.phone = this.quaRegForm.get('phoneNumber').value;
+      userData.position = {
+        latitude: this.currentLocation.lat as any,
+        longitude: this.currentLocation.lng as any,
+      };
+      userData.type = userType;
+      userData.placeId = this.userAddress.placeId;
+      // TODO : Generate 2 char short-code from user Input/make it a select dropdown ?
+      userData.country = this.userAddress.countryCode;
+      // userData.country = this.quaRegForm.get('country').value;
+    } else if (userType === 'HL') {
+      userData.type = userType;
+      userData.user = {
+        firstName: this.volRegForm.get('firstName').value,
+        lastName: this.volRegForm.get('lastName').value,
+        email: this.volRegForm.get('email').value,
+        password: this.volRegForm.get('password').value,
+      };
+      userData.phone = this.volRegForm.get('phoneNumber').value;
+    }
+
+    // start the loading animation
+    this.miscService
+      .presentLoadingWithOptions({
+        duration: 0,
+        message: `Registering user`,
+      })
+      .then((onLoadSuccess) => {
+        this.userRegAni = onLoadSuccess;
+        this.userRegAni.present();
+
+        console.log('userData', userData);
+        // call the register API
+        this.authService
+          .registerUser(userData)
+          .then((response: UserRegResponse) => {
+            this.userRegAni.dismiss();
+            this.router.navigate(['/login']);
+          })
+          .catch((errorObj) => {
+            this.userRegAni.dismiss();
+            const { error, status: statusCode } = errorObj;
+            const errorMessages: string[] = [];
+            for (const key in error) {
+              if (error.hasOwnProperty(key) && typeof key !== 'function') {
+                console.error(error[key][0]);
+                errorMessages.push(error[key][0]);
+              }
+            }
+            // show the errors as alert
+            this.handleLoginErrors(errorMessages, statusCode);
+          });
+      })
+      .catch((error) => alert(error));
   }
 
-  registerVolunteer() {
-    console.log(this.quaRegForm.value);
-    this.router.navigate(['/login']);
+  handleLoginErrors(errorMessages: string[], statusCode) {
+    console.log(...errorMessages, statusCode);
+    this.miscService.presentAlert({ message: errorMessages.join('. ') });
   }
 }
