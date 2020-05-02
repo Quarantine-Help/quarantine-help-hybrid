00110fde410f43ddb9da3dcbda0a4f413d5161a5
Sebin  Benjamin <sebinbenjamin@gmail.com>
2020-05-02 23:29:14 +1200

    feat: make a responsive search input for maps page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { AppComponent } from './app.component';
 import { AppMenuComponent } from './components/app-menu/app-menu.component';
 
 @NgModule({
-  declarations: [AppComponent,AppMenuComponent],
+  declarations: [AppComponent, AppMenuComponent],
   entryComponents: [],
   imports: [
     BrowserModule,
