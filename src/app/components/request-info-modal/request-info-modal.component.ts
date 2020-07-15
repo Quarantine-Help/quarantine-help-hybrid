931d2456d46c29ff908e49090dca6a45269d39af
Sebin  Benjamin <sebinbenjamin@gmail.com>
2020-07-15 21:13:20 +1200

    feat: complete modal UI for single request

--- src/app/components/request-info-modal/request-info-modal.component.ts
+++ src/app/components/request-info-modal/request-info-modal.component.ts
@@ -8,6 +8,14 @@ import { RequestView } from 'src/app/models/ui';
   styleUrls: ['./request-info-modal.component.scss'],
 })
 export class RequestInfoModalComponent implements OnInit {
+  request: {
+    createdAt: string;
+    deadline: string;
+    description: string;
+    id: number;
+    status: string;
+    type: string;
+  };
   constructor() {}
   // Data passed in by componentProps
   @Input() viewportX: string;
@@ -19,5 +27,15 @@ export class RequestInfoModalComponent implements OnInit {
     console.log(this.viewportX, this.viewportY);
     console.log('coordinates', this.coordinates);
     console.log('markerData', this.markerData);
+
+    // TEMP !!
+    this.request = {
+      createdAt: '2020-04-18T14:17:28.187947Z',
+      deadline: '2020-05-29T00:00:01Z',
+      description: 'Want 2 Kg ApplesWant 2 Kg ApplesWant 2 Kg Apples',
+      id: 5,
+      status: 'T',
+      type: 'M',
+    };
   }
 }
