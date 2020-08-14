c1db4a4bd26539c965d4a5159f0f7f1e2c330979
Nimisha Wilson <nimishawilson123@gmail.com>
2020-08-14 10:55:03 +0530

    feat: combine helpers and affected users profile page

--- src/app/pages/user-profile/user-profile.page.ts
+++ src/app/pages/user-profile/user-profile.page.ts
@@ -25,8 +25,7 @@ interface UserProfile {
   styleUrls: ['./user-profile.page.scss'],
 })
 export class UserProfilePage implements OnInit {
-  quaRegForm: FormGroup;
-  volRegForm: FormGroup;
+  profileForm: FormGroup;
   isVolunteer: boolean; // flag for current user type
   isEditable: boolean;
   loadingProfileData: HTMLIonLoadingElement;
@@ -40,22 +39,7 @@ export class UserProfilePage implements OnInit {
     private miscService: MiscService,
     private coreAPIService: CoreAPIService
   ) {
-    this.volRegForm = new FormGroup({
-      firstName: new FormControl('', [Validators.required]),
-      lastName: new FormControl('', [Validators.required]),
-      email: new FormControl('', [
-        Validators.required,
-        Validators.minLength(4),
-        Validators.email,
-      ]),
-      phoneNumber: new FormControl('', [
-        Validators.minLength(8),
-        Validators.maxLength(16),
-        Validators.required,
-      ]),
-    });
-
-    this.quaRegForm = new FormGroup({
+    this.profileForm = new FormGroup({
       firstName: new FormControl('', [Validators.required]),
       lastName: new FormControl('', [Validators.required]),
       address: new FormControl('', [
@@ -150,37 +134,22 @@ export class UserProfilePage implements OnInit {
     }
 
     // Restore previously fetched data if discarding changes
-    if (this.isVolunteer) {
-      this.volRegForm
-        .get('firstName')
-        .setValue(this.userProfileDetails.firstName);
-      this.volRegForm
-        .get('lastName')
-        .setValue(this.userProfileDetails.lastName);
-      this.volRegForm.get('email').setValue(this.userProfileDetails.emailid);
-      this.volRegForm
-        .get('phoneNumber')
-        .setValue(this.userProfileDetails.phoneNumber);
-    } else {
-      this.quaRegForm
-        .get('firstName')
-        .setValue(this.userProfileDetails.firstName);
-      this.quaRegForm
-        .get('lastName')
-        .setValue(this.userProfileDetails.lastName);
-      this.quaRegForm.get('address').setValue(this.userProfileDetails.address);
-      this.quaRegForm.get('email').setValue(this.userProfileDetails.emailid);
-      this.quaRegForm
-        .get('phoneNumber')
-        .setValue(this.userProfileDetails.phoneNumber);
-      this.quaRegForm.get('city').setValue(this.userProfileDetails.city);
-      this.quaRegForm.get('country').setValue(this.userProfileDetails.country);
-    }
+    this.profileForm
+      .get('firstName')
+      .setValue(this.userProfileDetails.firstName);
+    this.profileForm.get('lastName').setValue(this.userProfileDetails.lastName);
+    this.profileForm.get('address').setValue(this.userProfileDetails.address);
+    this.profileForm.get('email').setValue(this.userProfileDetails.emailid);
+    this.profileForm
+      .get('phoneNumber')
+      .setValue(this.userProfileDetails.phoneNumber);
+    this.profileForm.get('city').setValue(this.userProfileDetails.city);
+    this.profileForm.get('country').setValue(this.userProfileDetails.country);
   }
 
   onCancel() {
     this.isEditable = false;
-    if (this.quaRegForm.dirty || this.volRegForm.dirty) {
+    if (this.profileForm.dirty) {
       this.miscService.presentAlert({
         header: 'Warning',
         subHeader: 'Are you sure? ',
@@ -198,8 +167,7 @@ export class UserProfilePage implements OnInit {
             handler: () => {
               this.isEditable = false;
               this.syncDownProfileData();
-              this.volRegForm.markAsPristine();
-              this.quaRegForm.markAsPristine();
+              this.profileForm.markAsPristine();
             },
           },
         ],
@@ -209,15 +177,9 @@ export class UserProfilePage implements OnInit {
     this.searchResult.splice(0);
   }
 
-  saveQuaUser() {
+  saveUser() {
     this.isEditable = false;
-    this.quaRegForm.markAsPristine(); // for checking status of form in further edit and cancel
-    this.removeCleanFields();
-  }
-
-  saveVolUser() {
-    this.isEditable = false;
-    this.volRegForm.markAsPristine(); // for checking status of form in further edit and cancel
+    this.profileForm.markAsPristine(); // for checking status of form in further edit and cancel
     this.removeCleanFields();
   }
 
@@ -229,9 +191,9 @@ export class UserProfilePage implements OnInit {
   }
 
   setSelectedCountry(item) {
-    this.quaRegForm.markAsDirty();
+    this.profileForm.markAsDirty();
     this.isoAlphaTwoCode = item.isoAlphaTwoCode;
-    this.quaRegForm.patchValue({
+    this.profileForm.patchValue({
       country: item.name,
     });
     this.searchResult.splice(0);
@@ -247,69 +209,41 @@ export class UserProfilePage implements OnInit {
 
   // TODO: Refactor to use Form dirty
   removeCleanFields() {
-    if (this.isVolunteer) {
-      const volUserDetails = {
-        user: {
-          firstName:
-            this.volRegForm.get('firstName').value !==
-            this.userProfileDetails.firstName
-              ? this.volRegForm.get('firstName').value
-              : null,
-          lastName:
-            this.volRegForm.get('lastName').value !==
-            this.userProfileDetails.lastName
-              ? this.volRegForm.get('lastName').value
-              : null,
-          email:
-            this.volRegForm.get('email').value !==
-            this.userProfileDetails.emailid
-              ? this.volRegForm.get('email').value
-              : null,
-        },
-        phone:
-          this.volRegForm.get('phoneNumber').value !==
-          this.userProfileDetails.phoneNumber
-            ? this.volRegForm.get('phoneNumber').value
+    const quaUserDetails = {
+      user: {
+        firstName:
+          this.profileForm.get('firstName').value !==
+          this.userProfileDetails.firstName
+            ? this.profileForm.get('firstName').value
             : null,
-      };
-      this.syncUpProfileData(volUserDetails);
-    } else {
-      const quaUserDetails = {
-        user: {
-          firstName:
-            this.quaRegForm.get('firstName').value !==
-            this.userProfileDetails.firstName
-              ? this.quaRegForm.get('firstName').value
-              : null,
-          lastName:
-            this.quaRegForm.get('lastName').value !==
-            this.userProfileDetails.lastName
-              ? this.quaRegForm.get('lastName').value
-              : null,
-          email:
-            this.quaRegForm.get('email').value !==
-            this.userProfileDetails.emailid
-              ? this.quaRegForm.get('email').value
-              : null,
-        },
-        phone:
-          this.quaRegForm.get('phoneNumber').value !==
-          this.userProfileDetails.phoneNumber
-            ? this.quaRegForm.get('phoneNumber').value
+        lastName:
+          this.profileForm.get('lastName').value !==
+          this.userProfileDetails.lastName
+            ? this.profileForm.get('lastName').value
             : null,
-        firstLineOfAddress:
-          this.quaRegForm.get('address').value !==
-          this.userProfileDetails.address
-            ? this.quaRegForm.get('address').value
+        email:
+          this.profileForm.get('email').value !==
+          this.userProfileDetails.emailid
+            ? this.profileForm.get('email').value
             : null,
-        city:
-          this.quaRegForm.get('city').value !== this.userProfileDetails.city
-            ? this.quaRegForm.get('city').value
-            : null,
-        country: this.isoAlphaTwoCode,
-      };
-      this.syncUpProfileData(quaUserDetails);
-    }
+      },
+      phone:
+        this.profileForm.get('phoneNumber').value !==
+        this.userProfileDetails.phoneNumber
+          ? this.profileForm.get('phoneNumber').value
+          : null,
+      firstLineOfAddress:
+        this.profileForm.get('address').value !==
+        this.userProfileDetails.address
+          ? this.profileForm.get('address').value
+          : null,
+      city:
+        this.profileForm.get('city').value !== this.userProfileDetails.city
+          ? this.profileForm.get('city').value
+          : null,
+      country: this.isoAlphaTwoCode,
+    };
+    this.syncUpProfileData(quaUserDetails);
   }
 
   syncUpProfileData(profileDataDiff) {
