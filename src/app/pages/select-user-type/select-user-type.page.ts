6bf6fd171f4139418a41620c69f7aab056838947
Nimisha Wilson <nimishawilson123@gmail.com>
2020-10-31 11:48:03 +0530

    feat: add select option and buttons

--- src/app/pages/select-user-type/select-user-type.page.ts
+++ src/app/pages/select-user-type/select-user-type.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserThemeColorPrimary } from 'src/app/models/ui';
+
 
 @Component({
   selector: 'app-select-user-type',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./select-user-type.page.scss'],
 })
 export class SelectUserTypePage implements OnInit {
+  userThemeColorPrimary: UserThemeColorPrimary;
+  userThemeColorHL: UserThemeColorPrimary;
 
   constructor() { }
 
   ngOnInit() {
+    this.userThemeColorPrimary = 'primaryAF';
+    this.userThemeColorHL = 'primaryHL';
   }
 
 }
