bc86126171b5ee6ab56e5b85ce20328487470241
Nimisha Wilson <nimishawilson123@gmail.com>
2020-11-10 12:34:50 +0530

    feat: add navigation and small design changes

--- src/app/pages/select-user-type/select-user-type.page.ts
+++ src/app/pages/select-user-type/select-user-type.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { UserThemeColorPrimary } from 'src/app/models/ui';
-
 
 @Component({
   selector: 'app-select-user-type',
@@ -11,11 +11,23 @@ export class SelectUserTypePage implements OnInit {
   userThemeColorPrimary: UserThemeColorPrimary;
   userThemeColorHL: UserThemeColorPrimary;
 
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit() {
     this.userThemeColorPrimary = 'primaryAF';
     this.userThemeColorHL = 'primaryHL';
   }
 
+  goToAboutUs() {
+    console.log('navigate to about us page');
+    // this.router.navigateByUrl('/');
+  }
+
+  goToQuarantinedReg() {
+    this.router.navigateByUrl('/user-reg');
+  }
+
+  goToVolunteerReg() {
+    this.router.navigateByUrl('/user-reg');
+  }
 }
