411f96943b09fc4af2a9edcc06aa69a7da88270c
Naveen Thomas Varghese <naveen.panickal@gmail.com>
2020-11-11 13:59:15 +0530

    feat: complete prototype design

--- src/app/pages/about-us/about-us.module.ts
+++ src/app/pages/about-us/about-us.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
 
 import { AboutUsPageRoutingModule } from './about-us-routing.module';
-
 import { AboutUsPage } from './about-us.page';
 
 @NgModule({
