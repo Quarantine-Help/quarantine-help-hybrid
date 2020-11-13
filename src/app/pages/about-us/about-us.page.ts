a8036d2f74e5a9a6f865252b97ba51e67e71824b
Naveen Thomas Varghese <naveen.panickal@gmail.com>
2020-11-13 17:26:24 +0530

    fix: increase the line height of the text

--- src/app/pages/about-us/about-us.page.ts
+++ src/app/pages/about-us/about-us.page.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UserThemeColorPrimary } from 'src/app/models/ui';
+
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.page.html',
   styleUrls: ['./about-us.page.scss'],
 })
 export class AboutUsPage implements OnInit {
-
-  constructor() { }
+  userThemeColorPrimary: UserThemeColorPrimary;
+  constructor() {}
 
   ngOnInit() {
+    this.userThemeColorPrimary = 'primaryAF';
   }
-
 }
