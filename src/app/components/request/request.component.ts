02c3ae93be28eb07bbe28d12d484d1483c11c6cb
Nimisha Wilson <nimishawilson123@gmail.com>
2020-11-16 12:06:16 +0530

    feat: change segment color, request item color

--- src/app/components/request/request.component.ts
+++ src/app/components/request/request.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { RequestStatus } from 'src/app/constants/core-api';
 
 interface RequestId {
   id: string;
@@ -13,9 +14,19 @@ export class RequestComponent implements OnInit {
   @Output() requestOpened: EventEmitter<RequestId> = new EventEmitter<
     RequestId
   >();
+  requestStatusColor: UserThemeColorPrimary;
   constructor() {}
 
-  ngOnInit() {}
+
+  ngOnInit() {
+    console.log(this.requestData);
+
+    // TODO: Find out about the other statuses - C & F
+    this.requestStatusColor = 'primaryAF';
+    if (this.requestData.status === RequestStatus.transit) {
+      this.requestStatusColor = 'primaryHL';
+    }
+  }
 
   viewRequest(requestId) {
     this.requestOpened.emit({
