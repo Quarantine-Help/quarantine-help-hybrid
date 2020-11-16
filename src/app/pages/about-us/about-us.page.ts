be4c7f5e9d3f360c6fecd2be753642b26c722849
Naveen Thomas Varghese <naveen.panickal@gmail.com>
2020-11-17 01:15:24 +0530

    feat: complete the design of onboarding pages

--- src/app/pages/about-us/about-us.page.ts
+++ src/app/pages/about-us/about-us.page.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { defaultUserType } from 'src/app/constants/core-api';
+import { UserType } from 'src/app/models/core-api';
 
 import { UserThemeColorPrimary } from 'src/app/models/ui';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-about-us',
@@ -8,10 +12,22 @@ import { UserThemeColorPrimary } from 'src/app/models/ui';
   styleUrls: ['./about-us.page.scss'],
 })
 export class AboutUsPage implements OnInit {
+  userType: UserType;
+  authSubs: Subscription;
   userThemeColorPrimary: UserThemeColorPrimary;
-  constructor() {}
+
+  constructor(private authService: AuthService) {}
 
   ngOnInit() {
-    this.userThemeColorPrimary = 'primaryAF';
+    this.authSubs = this.authService.user.subscribe((user) => {
+      if (user && user.email !== undefined && user.token !== undefined) {
+        this.userType = user.type;
+      } else {
+        this.userType = defaultUserType;
+      }
+    });
+
+    this.userThemeColorPrimary =
+      this.userType === 'AF' ? 'primaryAF' : 'primaryHL';
   }
 }
