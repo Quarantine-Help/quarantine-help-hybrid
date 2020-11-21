30bcec2540b38b592f9eee46472066cc9bec5ef3
Sebin  Benjamin <sebinbenjamin@gmail.com>
2020-11-21 23:00:29 +1300

    feat: fix notification toggle color

--- src/app/pages/app-settings/app-settings.page.ts
+++ src/app/pages/app-settings/app-settings.page.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 
 import { UserThemeColorPrimary } from 'src/app/models/ui';
 import { UserType } from 'src/app/models/core-api';
 import { AuthService } from 'src/app/services/auth/auth.service';
-import { defaultUserType } from 'src/app/constants/core-api';
+import { defaultUserType, defaultPrimaryColor } from 'src/app/constants/core-api';
 
 @Component({
   selector: 'app-settings',
@@ -15,20 +16,35 @@ export class AppSettingsPage implements OnInit {
   userType: UserType;
   authSubs: Subscription;
   userThemeColorPrimary: UserThemeColorPrimary;
-  constructor(private authService: AuthService) {}
+  isLoggedIn: boolean;
+  constructor(private router: Router, private authService: AuthService) {}
 
   ngOnInit() {
-    this.userThemeColorPrimary = 'primaryAF';
-    this.userType = 'AF';
+    this.userThemeColorPrimary = defaultPrimaryColor;
+    this.userType = defaultUserType;
+    this.isLoggedIn = false;
 
     this.authSubs = this.authService.user.subscribe((user) => {
       if (user && user.email !== undefined && user.token !== undefined) {
         this.userType = user.type;
+        this.isLoggedIn = true;
         this.userThemeColorPrimary =
           this.userType === 'AF' ? 'primaryAF' : 'primaryHL';
       } else {
+        this.isLoggedIn = false;
         this.userType = defaultUserType;
+        this.userThemeColorPrimary = defaultPrimaryColor;
       }
     });
+  }
+
+  authAction() {
+    if (this.isLoggedIn) {
+      this.authService.logOutUser().then(() => {
+        this.router.navigateByUrl('/login');
+      });
+    } else {
+      this.router.navigateByUrl('/login');
+    }
   }
 }
