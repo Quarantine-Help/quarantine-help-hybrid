6f7bff33aa007038d68b5d03b0fc7583f9b15058
Naveen Thomas Varghese <naveen.panickal@gmail.com>
2020-11-23 15:27:39 +0530

    fix: correct the spacing of the imports in safety-intructions module

--- src/app/pages/safety-instructions/safety-instructions.module.ts
+++ src/app/pages/safety-instructions/safety-instructions.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
 
 import { SafetyInstructionsPageRoutingModule } from './safety-instructions-routing.module';
-
 import { SafetyInstructionsPage } from './safety-instructions.page';
 
 @NgModule({
