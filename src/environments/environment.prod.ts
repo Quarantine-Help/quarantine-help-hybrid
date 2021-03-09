99f6f3193ea080b45cd48cf731d07dac450d33eb
Sebin  Benjamin <sebinbenjamin@gmail.com>
2021-03-09 15:37:27 +1300

    chore: update domain in API url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   HERE_MAPS_JS_KEY: 'iY3ZFYqYj9Di2O4fuht-ilB7gONXpsYceJIsDgDbr94',
   HERE_MAPS_REST_KEY: 'INxGhspY9TqShx3heSZSBmobOsutPeE9eJaTxfHiiQQ',
-  DJANGO_API_ENDPOINT: `https://api.quarantinehelp.space/api`,
+  DJANGO_API_ENDPOINT: `https://api.quarantine-help.space/api`,
   DJANGO_API_VERSION: 1,
 };
